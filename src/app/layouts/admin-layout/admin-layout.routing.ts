3021660eea8cae8d490deecf9e8dd0874a51b608
Dilan-Fernando <dilanf@cbctechsol.com>
2022-01-13 11:59:31 +0530

    unused packages removed

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,22 +1,9 @@
 import {Routes} from '@angular/router';
 
-import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
+import {DashboardComponent} from '../../dashboard/dashboard.component';
 import {ApprovedComponent} from '../../approved/approved.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'pre-approval',      component: DashboardComponent },
-    { path: 'approved',   component: ApprovedComponent },
-    { path: 'table-list',     component: TableListComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent },
+    { path: 'approved',   component: ApprovedComponent }
 ];
